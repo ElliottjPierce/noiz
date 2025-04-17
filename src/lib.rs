ff6899dc67c1836cf8e730adc62ecfeb40e76853
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-17 15:27:43 -0400

    added AdaptiveNoise

--- src/lib.rs
+++ src/lib.rs
@@ -246,16 +246,6 @@ impl<
     }
 }
 
-/// This is the end goal of a [`NoiseFunction`].
-pub struct Noise<N> {
-    /// The [`NoiseFunction`] powering this noise.
-    pub noise: N,
-    /// The seed of the [`Noise`].
-    pub seed: RngContext,
-    /// The frequency or scale of the [`Noise`].
-    pub frequency: f32,
-}
-
 /// Specifies that this noise is configurable.
 pub trait ConfigurableNoise {
     /// Sets the seed of the noise as a `u64`.
@@ -281,6 +271,54 @@ pub trait ConfigurableNoise {
     }
 }
 
+/// Indicates that this noise is samplable by type `I`.
+pub trait Sampleable<I: VectorSpace> {
+    /// Represents the raw result of the sample.
+    type Result;
+
+    /// Samples the [`Noise`] at `loc`, returning the raw [`NoiseResult`] and the rng used for the sample.
+    /// This result may be incomplete and may depend on some cleanup to make the result meaningful.
+    /// Use this with caution.
+    fn sample_raw(&self, loc: I) -> (Self::Result, RngContext);
+
+    /// Samples the noise at `loc` for a result of type `T`. This is a convenience over [`SampleableFor`] since it doesn't require `T` to be written in the trait.
+    #[inline]
+    fn sample_for<T>(&self, loc: I) -> T
+    where
+        Self: SampleableFor<I, T>,
+    {
+        self.sample(loc)
+    }
+}
+
+/// Indicates that this noise is samplable by type `I` for type `T`.
+pub trait SampleableFor<I: VectorSpace, T> {
+    /// Samples the noise at `loc` for a result of type `T`.
+    fn sample(&self, loc: I) -> T;
+}
+
+/// A version of [`Sampleable`] that is object safe.
+/// `noize` uses exact types whenever possible to enable more inlining and optimizations,
+/// but this trait focuses instead on usability at the expense of speed.
+///
+/// Use [`Sampleable`] when you need performance and [`DynamicSampleable`] when you need object safety or don't want to bloat binary size with more inlining.
+pub trait DynamicSampleable<I: VectorSpace, T>: ConfigurableNoise + SampleableFor<I, T> {
+    /// This is the same as [`SampleableFor::sample`] but it is not inlined.
+    fn sample_dyn(&self, loc: I) -> T {
+        self.sample(loc)
+    }
+}
+
+/// This is the standard end interface of a [`NoiseFunction`].
+pub struct Noise<N> {
+    /// The [`NoiseFunction`] powering this noise.
+    pub noise: N,
+    /// The seed of the [`Noise`].
+    pub seed: RngContext,
+    /// The frequency or scale of the [`Noise`].
+    pub frequency: f32,
+}
+
 impl<N> ConfigurableNoise for Noise<N> {
     fn set_seed(&mut self, seed: u64) {
         self.seed = RngContext::from_bits(seed);
@@ -299,48 +337,85 @@ impl<N> ConfigurableNoise for Noise<N> {
     }
 }
 
-/// Indicates that this noise is samplable by type `I`.
-pub trait Sampleable<I: VectorSpace> {
-    /// Represents the raw result of the sample.
-    type Result;
-
-    /// Samples the [`Noise`] at `loc`, returning the raw [`NoiseResult`].
-    fn sample_raw(&self, loc: I) -> Self::Result;
-
-    /// Samples the noise at `loc` for a result of type `T`.
-    #[inline]
-    fn sample_for<T>(&self, loc: I) -> T
-    where
-        Self::Result: Into<T>,
-    {
-        self.sample_raw(loc).into()
-    }
-}
-
 impl<I: VectorSpace, N: NoiseFunction<I>> Sampleable<I> for Noise<N> {
     type Result = N::Output;
 
     #[inline]
-    fn sample_raw(&self, loc: I) -> Self::Result {
+    fn sample_raw(&self, loc: I) -> (Self::Result, RngContext) {
         let mut seeds = self.seed;
-        self.noise.evaluate(loc * self.frequency, &mut seeds)
+        let result = self.noise.evaluate(loc * self.frequency, &mut seeds);
+        (result, seeds)
     }
 }
 
-/// A version of [`Sampleable`] that is object safe.
-/// `noize` uses exact types whenever possible to enable more inlining and optimizations,
-/// but this trait focuses instead on usability at the expense of speed.
-///
-/// Use [`Sampleable`] when you need performance and [`DynamicSampleable`] when you need object safety or don't want to bloat binary size with more inlining.
-pub trait DynamicSampleable<I: VectorSpace, T>: ConfigurableNoise {
-    /// Samples the [`Noise`] at `loc`, returning the raw [`NoiseResult`].
-    fn sample_dyn(&self, loc: I) -> T;
+impl<T, I: VectorSpace, N: NoiseFunction<I, Output: Into<T>>> SampleableFor<I, T> for Noise<N> {
+    #[inline]
+    fn sample(&self, loc: I) -> T {
+        self.sample_raw(loc).0.into()
+    }
 }
 
-impl<T, I: VectorSpace, N: NoiseFunction<I, Output: Into<T>>> DynamicSampleable<I, T> for Noise<N> {
-    fn sample_dyn(&self, loc: I) -> T {
-        self.sample_for(loc)
+impl<T, I: VectorSpace, N> DynamicSampleable<I, T> for Noise<N> where
+    Self: SampleableFor<I, T> + Sampleable<I>
+{
+}
+
+/// This is a interface of a [`NoiseFunction`] that allows followup action in another [`NoiseFunction`] `A`.
+pub struct AdaptiveNoise<N, A> {
+    /// The [`Noise`] driving the initial result.
+    pub noise: Noise<N>,
+    /// The adapter finalizing the result.
+    pub adapter: A,
+}
+
+impl<N, A> ConfigurableNoise for AdaptiveNoise<N, A> {
+    fn set_seed(&mut self, seed: u64) {
+        self.noise.set_seed(seed);
     }
+
+    fn get_seed(&mut self) -> u64 {
+        self.noise.get_seed()
+    }
+
+    fn set_frequency(&mut self, frequency: f32) {
+        self.noise.set_frequency(frequency);
+    }
+
+    fn get_frequency(&mut self) -> f32 {
+        self.noise.get_frequency()
+    }
+
+    fn set_period(&mut self, period: f32) {
+        self.noise.set_period(period);
+    }
+
+    fn get_period(&mut self) -> f32 {
+        self.noise.get_period()
+    }
+}
+
+impl<I: VectorSpace, N: NoiseFunction<I>, A> Sampleable<I> for AdaptiveNoise<N, A> {
+    type Result = N::Output;
+
+    #[inline]
+    fn sample_raw(&self, loc: I) -> (Self::Result, RngContext) {
+        self.noise.sample_raw(loc)
+    }
+}
+
+impl<T, I: VectorSpace, N: NoiseFunction<I, Output: Into<T>>, A: NoiseFunction<T, Output = T>>
+    SampleableFor<I, T> for AdaptiveNoise<N, A>
+{
+    #[inline]
+    fn sample(&self, loc: I) -> T {
+        let (result, mut rng) = self.noise.sample_raw(loc);
+        self.adapter.evaluate(result.into(), &mut rng)
+    }
+}
+
+impl<T, I: VectorSpace, N, A> DynamicSampleable<I, T> for AdaptiveNoise<N, A> where
+    Self: SampleableFor<I, T> + Sampleable<I>
+{
 }
 
 /// Represents a [`NoiseOperationFor`] that contributes to the result via a [`NoiseFunction`] `T`.
