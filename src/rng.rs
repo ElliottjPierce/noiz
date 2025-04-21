fc18c3267ed9959eca59340df06ba55d0c42c1b2
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-21 00:16:32 -0400

    fixed bug

--- src/rng.rs
+++ src/rng.rs
@@ -57,7 +57,7 @@ impl NoiseRng {
         /// The base value bits for the floats we make.
         /// Positive sign, exponent of 0    , 16 value bits    7 bits as precision padding.
         const BASE_VALUE: u32 = 0b0_01111111_00000000_00000000_0111111;
-        const BIT_MASK: u32 = !((u16::MAX as u32) << 7);
+        const BIT_MASK: u32 = (u16::MAX as u32) << 7;
         let result = BASE_VALUE | (bits & BIT_MASK);
         f32::from_bits(result)
     }
