ce056309e2efe903022f1b756cf8935be16a369d
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-04-28 23:02:13 -0400

    optimized

--- src/math_noise.rs
+++ src/math_noise.rs
@@ -261,12 +261,13 @@ impl_mapped_vector_spaces!(Vec3A);
 impl_mapped_vector_spaces!(Vec4);
 
 /// A [`NoiseFunction`] that turns a cartesian cordinate into a polar cordinate.
+/// Contains a [`LengthFunction`] and a scale for radial cells.
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Spiral<L>(pub L);
+pub struct Spiral<L>(pub L, f32);
 
 impl<L: Default> Default for Spiral<L> {
     fn default() -> Self {
-        Self(L::default())
+        Self(L::default(), 1.0)
     }
 }
 
@@ -276,7 +277,7 @@ impl<L: LengthFunction<Vec2>> NoiseFunction<Vec2> for Spiral<L> {
     #[inline]
     fn evaluate(&self, input: Vec2, _seeds: &mut crate::rng::NoiseRng) -> Self::Output {
         let len = self.0.length_of(input);
-        let theta = (input.to_angle() * core::f32::consts::FRAC_1_PI) + 1.0;
+        let theta = input.to_angle() * core::f32::consts::FRAC_1_PI * self.1;
         Vec2::new(theta * len.floor(), len)
     }
 }
